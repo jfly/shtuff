37819bc76ea532d3c65e81f2a1f9a7c8609b7f0f
Jeremy Fleischman <jeremyfleischman@gmail.com>
2018-03-13 23:27:43 -0700

    Look at current git tag for versioning.

--- setup.py
+++ setup.py
@@ -9,8 +9,12 @@ from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
+import subprocess
 
 here = path.abspath(path.dirname(__file__))
+
+# Look at current git tag to get version.
+version = subprocess.check_output("git name-rev --tags --name-only $(git rev-parse HEAD)", shell=True).decode('utf-8').strip().lstrip("v")
 
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
@@ -39,7 +43,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.1',  # Required
+    version=version,  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
