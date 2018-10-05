724629cc3f7782e73751bb68217954915932e950
James LaChance <fatboyxpc@gmail.com>
2018-10-05 00:53:54 -0400

    Add support for multiple targets.
    Fixes #1.

--- shtuff.py
+++ shtuff.py
@@ -11,9 +11,12 @@ import argparse
 import subprocess
 import setproctitle
 
+from textwrap import dedent
 from pkg_resources import get_distribution, DistributionNotFound
 
-CONFIG_DIR = os.path.expanduser("~/.config/shtuff/")
+CONFIG_DIR = os.path.expanduser("~/.shtuff/")
+
+os.makedirs(CONFIG_DIR, exist_ok=True)
 
 try:
     __version__ = get_distribution(__name__).version
@@ -22,53 +25,89 @@ except DistributionNotFound:
     pass
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('to_stuff', type=str, nargs='?', default='', help='Keys to stuff')
-    parser.add_argument('-v', '--version', action='version', version=__version__)
-    parser.add_argument('--into', metavar="name", help='The name of the Shtuffee to run the command')
-    parser.add_argument('-n', dest="suppress_newline", action='store_true', help='Suppress new lines, similar to echo')
-    args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description="Shtuff will stuff commands into a shell à la tmux send-keys or screen stuff",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=dedent("""\
+            Examples:
+                In shell A, run:
+                  $ shtuff as shell-a
 
-    cmd = args.to_stuff
-    if not args.suppress_newline:
+                In shell B, run:
+                  $ shtuff into shell-a "git status"
+
+                Observe how shell A ran "git status"
+
+                An example use case for `shtuff new` might be a setup script to
+                open a couple shells automatically. Consider this script:
+                  #!/usr/bin/env bash
+                  termite -e "shtuff new vim" &
+                  termite -e "shtuff new 'tail -f /var/log/somelog.log'" &
+
+                This script will open two terminals, one running vim, and one
+                running tail.
+        """),
+    )
+    parser.add_argument('-v', '--version', action='version', version=__version__)
+    subparsers = parser.add_subparsers(metavar="action")
+
+    parser_as = subparsers.add_parser('as', help='become a receiving shell')
+    parser_as.add_argument('name', help='the current shell will take this name, use `shtuff into` to send commands here')
+    parser_as.set_defaults(func=shtuff_as)
+
+    def add_newline_argument(parser):
+        parser.add_argument('-n', dest="newline", action='store_false', help="don't send a trailing newline, similar to echo")
+
+    parser_into = subparsers.add_parser('into', help='send a command to a receiving shell')
+    parser_into.add_argument('name', help='the name of the shell to send the given command to')
+    parser_into.add_argument('cmd', help='the command to send to the shell')
+    add_newline_argument(parser_into)
+    parser_into.set_defaults(func=shtuff_into)
+
+    parser_new = subparsers.add_parser('new', help='start a new shell and immediately send a command')
+    parser_new.add_argument('cmd', help='start a new shell and immediately run the given command')
+    add_newline_argument(parser_new)
+    parser_new.set_defaults(func=shtuff_new)
+
+
+    args = vars(parser.parse_args())
+    func = args.pop('func')
+    func(**args)
+
+def shtuff_as(name):
+    pid_file = get_pid_file(name)
+    pid = find_nearest_shtuff_process()
+
+    if not pid:
+        spawn_and_stuff(os.environ['SHELL'], f'shtuff as {name}\n')
+
+    with open(pid_file, 'w') as f:
+        f.write(str(pid))
+
+def shtuff_into(name, cmd, newline):
+    if newline:
         cmd += "\n"
 
-    if args.into is None:
-        spawn_and_stuff(os.environ['SHELL'], cmd)
-    else:
-        stuff(args.into, cmd)
-
-def stuff(target, to_stuff):
-    pid_file = get_pid_file(target)
+    pid_file = get_pid_file(name)
     with open(pid_file) as f:
         pid = int(f.read().strip())
 
     with open(get_cmd_file(pid), 'w') as f:
-        f.write(to_stuff)
+        f.write(cmd)
 
     os.kill(pid, signal.SIGUSR1)
+
+def shtuff_new(cmd, newline):
+    if newline:
+        cmd += "\n"
+
+    spawn_and_stuff(os.environ['SHELL'], cmd)
 
 def get_pid_file(name):
     return os.path.join(CONFIG_DIR, "{name}.pid".format(name=name))
 
 def get_cmd_file(pid):
     return os.path.join(CONFIG_DIR, "{pid}.command".format(pid=pid))
-
-def shtuffee():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('name', help='The name of the Shtuffee to run the command')
-    parser.add_argument('-v', '--version', action='version', version=__version__)
-
-    name = parser.parse_args().name
-    pid_file = get_pid_file(name)
-    pid = find_nearest_shtuff_process()
-
-    if not pid:
-        spawn_and_stuff(os.environ['SHELL'], f'shtuffee {name}\n')
-
-    with open(pid_file, 'w') as f:
-        f.write(str(pid))
-
 
 def find_nearest_shtuff_process():
     return subprocess.run(
