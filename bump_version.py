ab9e715c6457bb40badc6ea2df399cbd289048e2
James LaChance <fatboyxpc@gmail.com>
2018-10-25 02:03:10 -0400

    Changes per @jfly's feedback

--- bump_version.py
+++ bump_version.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import sys
+
 def bump_version(version, level):
     major, minor, patch = map(int, version.lstrip('v').split('.'))
 
@@ -10,6 +12,9 @@ def bump_version(version, level):
     }
 
     return "v%d.%d.%d" % semver[level]
+
+def main():
+    print(bump_version(sys.argv[1], sys.argv[2]))
 
 if __name__ == "__main__":
     main()
--- release.sh
+++ release.sh
@@ -2,28 +2,24 @@
 
 RELEASE_LEVEL=$1
 
-git remote -v | grep "^upstream.*github\.com.jfly\/shtuff\.git" &> /dev/null
-
-if [[ $? -gt 0 ]]; then
+if ! git remote -v | grep "^upstream.*github\.com.jfly\/shtuff\.git" &> /dev/null; then
     echo "A remote named upstream must be configured to point to https://github.com/jfly/shtuff.git to be able to make a release"
     exit 1
 fi
 
-git fetch upstream &> /dev/null
-
-if [[ $? -gt 0 ]]; then
-    echo "Fetch did not perform successfully, make sure you hav permission to pull from this repository."
+if ! git fetch upstream &> /dev/null; then
+    echo "Fetch did not perform successfully, make sure you have permission to pull from this repository."
     exit 2
 fi
 
 CURRENT_VERSION=$(git tag | sort -V | tail -n 1)
-NEW_VERSION=$(python -c "from bump_version import bump_version; print(bump_version('$CURRENT_VERSION', '$RELEASE_LEVEL'))")
+NEW_VERSION=$(./bump_version.py "$CURRENT_VERSION" "$RELEASE_LEVEL")
 
 git tag $NEW_VERSION
 git push --tags upstream
 
 if [[ $? -gt 0 ]]; then
-    echo "Push did not perform successfully, make sure you hav permission to push to this repository."
+    echo "Push did not perform successfully, make sure you have permission to push to this repository."
     exit 2
 fi
 
