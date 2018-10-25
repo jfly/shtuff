607865888666af6275ae7d9acea1ca263a2a1357
James LaChance <fatboyxpc@gmail.com>
2018-10-25 01:19:39 -0400

    Strengthen tests from @jfly's feedback
    @jfly pointed out that using 0's in tests is a little scary because we
    could end up resetting a 0 in the actual code and not realize it in the
    tests. He pointed out that patch: (major, 0, patch + 1) would make the
    tests pass, but they shouldn't! I decided to use v.1.2.3 as the starting
    version, subbing 9 where applicable for the "carry over" tests. I
    realized that test_bump_major and test_bump_minor were redundant at that
    point.
    
    Good eye, @jfly!

--- tests/test_bump_version.py
+++ tests/test_bump_version.py
@@ -3,22 +3,16 @@ from bump_version import bump_version
 
 class TestBumpVersion(unittest.TestCase):
     def test_bump_patch(self):
-        self.assertEqual(bump_version('v1.0.0', 'patch'), 'v1.0.1')
+        self.assertEqual(bump_version('v1.2.3', 'patch'), 'v1.2.4')
 
     def test_bump_patch_does_not_carry_over(self):
-        self.assertEqual(bump_version('v1.0.9', 'patch'), 'v1.0.10')
-
-    def test_bump_minor(self):
-        self.assertEqual(bump_version('v1.0.0', 'minor'), 'v1.1.0')
+        self.assertEqual(bump_version('v1.2.9', 'patch'), 'v1.2.10')
 
     def test_bump_minor_resets_patch(self):
-        self.assertEqual(bump_version('v1.0.1', 'minor'), 'v1.1.0')
+        self.assertEqual(bump_version('v1.2.3', 'minor'), 'v1.3.0')
 
     def test_bump_minor_does_not_carry_over(self):
-        self.assertEqual(bump_version('v1.9.0', 'minor'), 'v1.10.0')
-
-    def test_bump_major(self):
-        self.assertEqual(bump_version('v1.0.0', 'major'), 'v2.0.0')
+        self.assertEqual(bump_version('v1.9.3', 'minor'), 'v1.10.0')
 
     def test_bump_major_resets_minor_and_patch(self):
-        self.assertEqual(bump_version('v1.1.1', 'major'), 'v2.0.0')
+        self.assertEqual(bump_version('v1.2.3', 'major'), 'v2.0.0')
